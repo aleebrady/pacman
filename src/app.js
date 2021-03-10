35e07995152dd7b0d40f2a1194b1520260bf317f
aleebrady <aleebrady@gmail.com>
2021-03-10 14:06:53 -0800

    creating grid

---
+++ src/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const score = document.getElementById('score')
+    const width = 28 // 28 x 28 = 784 squares
+
+// layout of the grid and what is in the squares
+
+})
