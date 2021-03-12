af59a7558637a5a27cbc7522518906854a16a9c6
aleebrady <aleebrady@gmail.com>
2021-03-12 14:35:41 -0800

    added eventlistener movement for game

--- src/app.js
+++ src/app.js
@@ -56,6 +56,8 @@ const squares = []
               squares[i].classList.add('pac-dot')
           } else if (layout[i] === 1) {
               squares[i].classList.add('wall')
+          } else if (layout[i] === 2) {
+              squares[i].classList.add('ghost-lair')
           } else if (layout[i] === 3) {
               squares[i].classList.add('power-pellet')
           } 
@@ -75,19 +77,35 @@ function movePacman(event) {
 
     switch(event.keyCode) {
         case 37:
-            if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+            if (pacmanCurrentIndex % width !== 0 && 
+                !squares[pacmanCurrentIndex -1].classList.contains('wall') && 
+                !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')) pacmanCurrentIndex -=1
             break
         case 38:
-            if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -=width
+            if (pacmanCurrentIndex - width >= 0 && 
+                !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
+                !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')) pacmanCurrentIndex -=width
             break
         case 39:
-            if (pacmanCurrentIndex % width < width -1) pacmanCurrentIndex +=1
+            if (pacmanCurrentIndex % width < width -1 && 
+                !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+                !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')) pacmanCurrentIndex +=1
             break
         case 40:
-            if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex +=width
+            if(pacmanCurrentIndex + width < width * width && 
+                !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
+                !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')) pacmanCurrentIndex +=width
             break
     }
 
     squares[pacmanCurrentIndex].classList.add('pac-man')
+
+    // create functions for pacDotEaten
+    // powerPelletEaten
+    //forGameOver
+    // gameWon
+
 }
+document.addEventListener('keyup', movePacman)
+
 })
